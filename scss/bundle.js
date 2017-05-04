0bd4a92f7374184759258de04f5e509030b46d65
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-03 19:09:36 -0700

    Rename variable names for gulp plugins

--- scss/bundle.js
+++ scss/bundle.js
@@ -1,13 +1,13 @@
 'use strict'
 
-var autoprefixer = require('gulp-autoprefixer')
+var gulpAutoprefixer = require('gulp-autoprefixer')
 var deepExtend = require('deep-extend')
 var fs = require('fs')
 var gulp = require('gulp')
-var rename = require('gulp-rename')
-var rev = require('gulp-rev')
-var sass = require('gulp-sass')
-var sourcemaps = require('gulp-sourcemaps')
+var gulpRename = require('gulp-rename')
+var gulpRev = require('gulp-rev')
+var gulpSass = require('gulp-sass')
+var gulpSourcemaps = require('gulp-sourcemaps')
 var exceptions = require('../exceptions')
 var IllegalArgumentException = exceptions.IllegalArgumentException
 var FileDoesNotExistException = exceptions.FileDoesNotExistException
@@ -35,22 +35,22 @@ function bundleScss(entryPath, bundlePath, options) {
   return new Promise((resolve, reject) => {
     var stream = gulp.src(entryPath)
     if (options.sourcemaps) {
-      stream = stream.pipe(sourcemaps.init())
+      stream = stream.pipe(gulpSourcemaps.init())
     }
     stream = stream.pipe(
-      sass({ outputStyle: 'compressed' })
+      gulpSass({ outputStyle: 'compressed' })
       .on('error', function onSassError(error) {
-        sass.logError.call(this, error)
+        gulpSass.logError.call(this, error)
         reject()
       })
     )
-    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-    .pipe(rename(bundlePath))
+    .pipe(gulpAutoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(gulpRename(bundlePath))
     if (options.rev) {
-      stream = stream.pipe(rev())
+      stream = stream.pipe(gulpRev())
     }
     if (options.sourcemaps) {
-      stream = stream.pipe(sourcemaps.write('.'))
+      stream = stream.pipe(gulpSourcemaps.write('.'))
     }
     stream.pipe(gulp.dest('.'))
     .on('finish', () => {
