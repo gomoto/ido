ac2337525afb2704fa90bb0581d60429b079e341
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-04 18:51:19 -0700

    Bundle index.html

---
+++ html/bundle.js
@@ -0,0 +1,58 @@
+'use strict'
+
+var deepExtend = require('deep-extend')
+var fs = require('fs')
+var gulp = require('gulp')
+var htmlInjector = require('html-injector')
+var htmlMinifierStream = require('html-minifier-stream')
+var gulpRevReplace = require('gulp-rev-replace')
+var vinylBuffer = require('vinyl-buffer')
+var vinylSourceStream = require('vinyl-source-stream')
+var exceptions = require('../exceptions')
+var IllegalArgumentException = exceptions.IllegalArgumentException
+
+/**
+ * Bundle index.html.
+ * @param {string} entryPath
+ * @param {string} bundlePath
+ * @param {Object} options
+ * @return {Promise}
+ */
+function bundleHtml(entryPath, bundlePath, options) {
+  if (typeof entryPath !== 'string') throw new IllegalArgumentException('entryPath')
+  if (typeof bundlePath !== 'string') throw new IllegalArgumentException('bundlePath')
+
+  options = deepExtend({
+    inject: {},
+    minify: true,
+    revManifestPaths: []
+  }, options)
+
+  return new Promise((resolve, reject) => {
+    var stream = fs.createReadStream(entryPath)
+    if (options.inject) {
+      stream = stream.pipe(htmlInjector(options.inject))
+    }
+    if (options.minify) {
+      stream = stream.pipe(htmlMinifierStream({
+        collapseWhitespace: true,
+        processScripts: ['text/ng-template']
+      }))
+    }
+    stream = stream.pipe(vinylSourceStream(bundlePath))
+    .pipe(vinylBuffer())
+
+    // Replace revision hashes.
+    if (options.revManifestPaths) {
+      stream = stream.pipe(gulpRevReplace({
+        manifest: gulp.src(options.revManifestPaths)
+      }))
+    }
+    stream.pipe(gulp.dest('.'))
+    .on('finish', () => {
+      resolve()
+    })
+  })
+}
+
+module.exports = bundleHtml
