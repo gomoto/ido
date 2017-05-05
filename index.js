850c6e9c13cd196b3f7ebbffbf0cabcb1d9d1a4b
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-04 21:53:47 -0700

    Import modules into index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+var bower = require('./bower')
+var file = require('./file')
+var html = require('./html')
+var image = require('./image')
+var npm = require('./npm')
 var scss = require('./scss')
 var typescript = require('./typescript')
 
