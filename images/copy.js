10860d62bad6879cf76a59556494d477eba4f394
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-04 18:06:19 -0700

    Copy images, optionally minifying and revisioning them

---
+++ images/copy.js
@@ -0,0 +1,47 @@
+'use strict'
+
+var deepExtend = require('deep-extend')
+var gulp = require('gulp')
+var gulpImagemin = require('gulp-imagemin')
+var gulpRev = require('gulp-rev')
+var exceptions = require('../exceptions')
+var IllegalArgumentException = exceptions.IllegalArgumentException
+
+/**
+ * Copy images, optionally minifying and revisioning them.
+ * @param  {string} srcGlob glob for source files
+ * @param  {string} destDir destination directory
+ * @param  {Object} options
+ * @return {Promise}
+ */
+function copyImages(srcGlob, destDir, options) {
+  if (typeof srcGlob !== 'string') throw new IllegalArgumentException('srcGlob')
+  if (typeof destDir !== 'string') throw new IllegalArgumentException('destDir')
+
+  options = deepExtend({
+    // image manifest location is relative to destDir
+    manifest: '',
+    minify: true,
+    rev: true
+  }, options)
+
+  return new Promise((resolve, reject) => {
+    var stream = gulp.src(srcGlob)
+    if (options.minify) {
+      stream = stream.pipe(gulpImagemin())
+    }
+    if (options.rev) {
+      stream = stream.pipe(gulpRev())
+    }
+    stream = stream.pipe(gulp.dest(destDir))
+    if (options.manifest) {
+      stream = stream.pipe(gulpRev.manifest(options.manifest))
+      .pipe(gulp.dest(destDir))
+    }
+    stream.on('finish', () => {
+      resolve()
+    })
+  })
+}
+
+module.exports = copyImages
