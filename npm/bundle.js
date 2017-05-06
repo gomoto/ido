8c6a13463197f34bc1affe3f177b7a8e8a930176
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-05 17:13:54 -0700

    No rev manifest in npm task

--- npm/bundle.js
+++ npm/bundle.js
@@ -7,7 +7,6 @@ var gulp = require('gulp')
 var gulpRev = require('gulp-rev')
 var gulpSourcemaps = require('gulp-sourcemaps')
 var gulpUglify = require('gulp-uglify')
-var mergeStream = require('merge-stream')
 var path = require('path')
 var vinylSourceStream = require('vinyl-source-stream')
 var exceptions = require('../exceptions')
@@ -58,6 +57,13 @@ function _createBundle(entryPath, options) {
  * @return {Promise}
  */
 function _bundle(browserifyBundle, bundlePath, options) {
+  var metadata = {
+    bundle: {
+      name: '',
+      originalName: path.basename(bundlePath)
+    }
+  }
+
   return new Promise((resolve, reject) => {
     var stream = browserifyBundle.bundle()
     .on('error', (err) => {
@@ -74,20 +80,17 @@ function _bundle(browserifyBundle, bundlePath, options) {
     if (options.rev) {
       stream = stream.pipe(gulpRev())
     }
+    // Record bundle name.
+    // Do this before gulp-sourcemaps adds a file to the stream.
+    stream.on('data', (file) => {
+      metadata.bundle.name = path.basename(file.path)
+    })
     if (options.sourcemaps) {
       stream = stream.pipe(gulpSourcemaps.write('.'))
     }
-    stream = stream.pipe(gulp.dest('.'))
-
-    // Record rev in manifest.
-    if (options.manifest) {
-      var manifestStream = stream
-      .pipe(gulpRev.manifest(options.manifest))
-      .pipe(gulp.dest(process.cwd()))
-      stream = mergeStream(stream, manifestStream)
-    }
-    stream.on('finish', () => {
-      resolve()
+    stream.pipe(gulp.dest('.'))
+    .on('finish', () => {
+      resolve(metadata)
     })
   })
 }
@@ -104,7 +107,6 @@ function bundleNpm(entryPath, bundlePath, options) {
   if (typeof bundlePath !== 'string') throw new IllegalArgumentException('bundlePath')
 
   options = deepExtend({
-    manifest: '',
     rev: false,
     sourcemaps: false,
     minify: false
